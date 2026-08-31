7d88cb9e3cc67c10432b18c76c5d69cdd1b5cf9b
Pasha <pasha29080@gmail.com>
2026-08-31 09:54:27 +0700

    manambahakan transaksi_tambah.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host     = "localhost";
 $user     = "root";
-$password = ""; 
-$database = "db_laundry";
+$password = "";
+$database = "laundry\"rr\"";
 
 $koneksi = mysqli_connect($host, $user, $password, $database);
 
