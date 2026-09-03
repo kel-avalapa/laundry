9f7d9ff968151805e631be4e5b7ba6cafbef7a46
Pasha <pasha29080@gmail.com>
2026-09-03 18:18:11 +0700

    fix koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,8 @@
 <?php
-$host     = "localhost";
-$user     = "root";
+$host = "localhost";
+$user = "root";
 $password = "";
-$database = "laundry\"rr\"";
-=======
-$password = ""; 
-<<<<<<< HEAD
-$database = "laundry"rr"";
-
->>>>>>> 2f76d0d11030b8521d35c61094235b3eb63a0244
+$database = "laundry";
 
 $koneksi = mysqli_connect($host, $user, $password, $database);
 
