881ab11353f6b9b62813257808dedda8b44a8b14
Pasha <pasha29080@gmail.com>
2026-09-03 18:03:24 +0700

    fix koneksi dan upload pelanggan.php

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,8 @@
 <?php
 $host     = "localhost";
 $user     = "root";
-<<<<<<< HEAD
 $password = "";
-$database = "laundry\"rr\"";
-=======
-$password = ""; 
-$database = "laundry"rr"";
->>>>>>> 2efb79e41183003471388576f431b9d3761221c7
+$database = "laundry";
 
 $koneksi = mysqli_connect($host, $user, $password, $database);
 
