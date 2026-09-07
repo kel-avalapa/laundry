e0aa8aae26710d9abf0ff5cf2611d858a4df023e
rulaanaflah <rulaanaflah@gmail.com>
2026-09-07 12:51:48 +0700

    fix error dan sesuaikan struktur di pelanggan_edit.php

--- pelanggan_edit.php
+++ pelanggan_edit.php
@@ -1,46 +1,63 @@
-PHP
 <?php
 include 'koneksi.php';
+
+// Cek apakah ada parameter ID di URL
+if (!isset($_GET['id'])) {
+    header("location:pelanggan.php");
+    exit();
+}
+
 $id = $_GET['id'];
-$query = mysqli_query($koneksi, "SELECT * FROM tb_pelanggan WHERE id_pelanggan = '$id'");
-$d = mysqli_fetch_array($query);
+
+// Ambil data pelanggan berdasarkan id_pelanggan
+$query = "SELECT * FROM tb_pelanggan WHERE id_pelanggan = '$id'";
+$result = mysqli_query($koneksi, $query);
+$d = mysqli_fetch_array($result);
+
+// Jika tombol simpan perubahan diklik
+if (isset($_POST['update'])) {
+    $nama = $_POST['nama'];
+    $no_hp = $_POST['no_hp'];
+    $alamat = $_POST['alamat'];
+
+    $update_query = "UPDATE tb_pelanggan SET nama='$nama', no_hp='$no_hp', alamat='$alamat' WHERE id_pelanggan='$id'";
+    mysqli_query($koneksi, $update_query) or die(mysqli_error($koneksi));
+
+    header("location:pelanggan.php");
+    exit();
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Edit Pelanggan - Laundry</title>
+    <title>Edit Data Pelanggan Laundry "RR"</title>
 </head>
 <body>
     <h2>Edit Data Pelanggan</h2>
-    <a href="pelanggan.php">KEMBALI</a><br><br>
+    <a href="pelanggan.php">&laquo; Kembali</a><br><br>
 
-    <form method="post" action="pelanggan_update.php">
-        <input type="hidden" name="id_pelanggan" value="<?= $d['id_pelanggan']; ?>">
-        <table border="0">
+    <form method="POST" action="">
+        <table>
             <tr>
                 <td>Nama</td>
-                <td><input type="text" name="nama" value="<?= $d['nama']; ?>" required></td>
+                <td>:</td>
+                <td><input type="text" name="nama" value="<?= isset($d['nama']) ? $d['nama'] : ''; ?>" required></td>
             </tr>
             <tr>
                 <td>Alamat</td>
-                <td><textarea name="alamat" required><?= $d['alamat']; ?></textarea></td>
+                <td>:</td>
+                <td><textarea name="alamat" required><?= isset($d['alamat']) ? $d['alamat'] : ''; ?></textarea></td>
             </tr>
             <tr>
-                <td>Jenis Kelamin</td>
-                <td>
-                    <select name="jenis_kelamin">
-                        <option value="L" <?= $d['jenis_kelamin'] == 'L' ? 'selected' : ''; ?>>Laki-laki</option>
-                        <option value="P" <?= $d['jenis_kelamin'] == 'P' ? 'selected' : ''; ?>>Perempuan</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>No. Telepon</td>
-                <td><input type="text" name="tlp" value="<?= $d['tlp']; ?>" required></td>
+                <td>No. HP</td>
+                <td>:</td>
+                <td><input type="text" name="no_hp" value="<?= isset($d['no_hp']) ? $d['no_hp'] : ''; ?>" required></td>
             </tr>
             <tr>
                 <td></td>
-                <td><input type="submit" value="SIMPAN PERUBAHAN"></td>
+                <td></td>
+                <td><button type="submit" name="update">Simpan Perubahan</button></td>
             </tr>
         </table>
     </form>
