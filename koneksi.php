93582029daee9bfbd6831cb89e1811236bdaf072
Pasha <pasha29080@gmail.com>
2026-09-07 11:28:23 +0700

    fix koneksi database

--- koneksi.php
+++ koneksi.php
@@ -2,8 +2,11 @@
 $host     = "localhost";
 $user     = "root";
 $password = "";
-$database = "laundry\"rr\""; // Tanda \ wajib ada sebelum petik ganda
-git
+$database = 'laundry"rr"';
+
+$koneksi  = mysqli_connect($host, $user, $password, $database);
+
+if (mysqli_connect_errno()) {
     echo "Koneksi database gagal: " . mysqli_connect_error();
 }
 ?>
