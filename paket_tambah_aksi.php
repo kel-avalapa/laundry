5213cc34f7f465fbf4e7ba75862eff37e402fd56
rulaanaflah <rulaanaflah@gmail.com>
2026-09-14 09:53:22 +0700

    fix proses tambah paket dan aksi simpan

--- paket_tambah_aksi.php
+++ paket_tambah_aksi.php
@@ -1,10 +1,19 @@
 <?php
 include 'koneksi.php';
 
-$jenis      = $_POST['jenis'];
-$nama_paket = $_POST['nama_paket'];
-$harga      = $_POST['harga'];
+// Pastikan tombol atau proses berjalan
+if (isset($_POST['jenis_paket']) || isset($_POST['nama_paket'])) {
+    $jenis_paket = $_POST['jenis_paket'];
+    $nama_paket  = $_POST['nama_paket'];
+    $harga       = $_POST['harga'];
 
-mysqli_query($koneksi, "INSERT INTO tb_paket VALUES('', '$jenis', '$nama_paket', '$harga')");
-header("location:paket.php");
+    // Masukkan ke database (sesuaikan nama kolom tabel tb_paket)
+    $query = "INSERT INTO tb_paket (jenis_paket, nama_paket, harga) VALUES ('$jenis_paket', '$nama_paket', '$harga')";
+    $insert = mysqli_query($koneksi, $query) or die(mysqli_error($koneksi));
+
+    if ($insert) {
+        header("location:paket.php");
+        exit();
+    }
+}
 ?>
