0f334c5226c2da8bddc0d7f270f9953fbe637f91
rulaanaflah <rulaanaflah@gmail.com>
2026-09-14 09:14:46 +0700

    fix error undefined array key di paket.php

--- paket.php
+++ paket.php
@@ -1,38 +1,46 @@
-PHP
 <?php
 include 'koneksi.php';
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Data Paket - Laundry</title>
+    <title>Data Paket Laundry "RR"</title>
 </head>
 <body>
     <h2>Data Paket Laundry</h2>
     <a href="paket_tambah.php">+ Tambah Paket</a><br><br>
 
-    <table border="1" cellpadding="8">
+    <table border="1" cellpadding="8" cellspacing="0">
         <tr>
             <th>No</th>
             <th>Nama Paket</th>
             <th>Jenis</th>
             <th>Harga/Kg</th>
+            <th>Aksi</th>
         </tr>
         <?php
         $no = 1;
-        $query = mysqli_query($koneksi, "SELECT * FROM tb_paket");
-        while($d = mysqli_fetch_array($query)){
+        $query = "SELECT * FROM tb_paket ORDER BY id_paket DESC";
+        $data = mysqli_query($koneksi, $query) or die(mysqli_error($koneksi));
+        
+        while ($d = mysqli_fetch_array($data)) {
+            // Menyesuaikan jika nama kolom di database menggunakan 'harga' atau 'harga_paket'
+            $harga_val = isset($d['harga']) ? $d['harga'] : (isset($d['harga_paket']) ? $d['harga_paket'] : 0);
+            
+            // Menyesuaikan jika nama kolom jenis menggunakan 'jenis_paket' atau 'jenis'
+            $jenis_val = isset($d['jenis_paket']) ? $d['jenis_paket'] : (isset($d['jenis']) ? $d['jenis'] : '-');
         ?>
         <tr>
-    <td><?= $no++; ?></td>
-    <td><?= $d['nama_paket']; ?></td>
-    <td><?= $d['jenis_paket']; ?></td>
-    <td>Rp <?= number_format($d['harga_paket']); ?></td>
-    <td>
-        <a href="paket_edit.php?id_paket=<?= $d['id_paket']; ?>">Edit</a> | 
-        <a href="paket_hapus.php?id_paket=<?= $d['id_paket']; ?>">Hapus</a>
-    </td>
-</tr>
+            <td><?= $no++; ?></td>
+            <td><?= isset($d['nama_paket']) ? $d['nama_paket'] : '-'; ?></td>
+            <td><?= ucfirst($jenis_val); ?></td>
+            <td>Rp <?= number_format($harga_val, 0, ',', '.'); ?></td>
+            <td>
+                <a href="paket_edit.php?id=<?= $d['id_paket']; ?>">Edit</a> | 
+                <a href="paket_hapus.php?id=<?= $d['id_paket']; ?>" onclick="return confirm('Yakin ingin menghapus paket ini?')">Hapus</a>
+            </td>
+        </tr>
         <?php } ?>
     </table>
 </body>
